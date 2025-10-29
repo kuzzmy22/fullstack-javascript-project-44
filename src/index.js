f18ee314a50ad113faa1d64c28949e10d49e4c27
Daniil <kuzzmy@yandex.ru>
2025-10-29 20:53:14 +0300

    rules other

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+
+const ROUNDS_COUNT = 3;
+
+const runGame = (description, generateRound) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  
+  if (description) {
+    console.log(description);
+  }
+
+  for (let i = 0; i < ROUNDS_COUNT; i += 1) {
+    const [question, correctAnswer] = generateRound();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer.toString()) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default runGame;
